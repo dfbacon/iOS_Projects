852a6f5ec6bed885458752a84eb5c56a7bf7669d
Daniel Bacon <dbacon338@gmail.com>
2017-06-09 11:24:10 -0700

    update pods

--- Social/Social/AppDelegate.swift
+++ Social/Social/AppDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Firebase
 
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -43,7 +44,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    
 }
 
