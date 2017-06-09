0e1598e64977f45449b5c4f4548e8524c968e8e7
Daniel Bacon <dbacon338@gmail.com>
2017-06-09 14:45:24 -0700

    updates to feed page; unfinished

--- Social/Social/RoundView.swift
+++ Social/Social/RoundView.swift
@@ -1,27 +1,29 @@
 //
-//  ModificationView.swift
+//  RoundView.swift
 //  Social
 //
-//  Created by Daniel Bacon on 6/8/17.
+//  Created by Daniel Bacon on 6/9/17.
 //  Copyright © 2017 Daniel Bacon. All rights reserved.
 //
 
 import UIKit
 
-class ModificationView: UIView {
+class RoundView: UIImageView {
 
-    // Add shadow/depth to header
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        // SHADOW_GRAY defined as global constant in Constants.swift
         layer.shadowColor = UIColor(displayP3Red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
-
-        // How far the shadow expands out
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        
-        layer.cornerRadius = 2.0
+
     }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        layer.cornerRadius = self.frame.width / 2
+    }
+    
 }
