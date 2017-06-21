734e3de5ec94cc8d40c1451e9d467515b9ce431a
Daniel Bacon <dbacon338@gmail.com>
2017-06-21 10:36:33 -0700

    setup with apple source code

--- DevChat/DevChat/CameraVC.swift
+++ DevChat/DevChat/CameraVC.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CameraVC: AAPLCameraViewController {
 
+    @IBOutlet weak var previewView: AAPLPreviewView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
