ca4edc4399d796cd845207862dacba2ee041a40c
Daniel Bacon <dbacon338@gmail.com>
2017-08-23 15:52:26 -0700

    add framework for Wall, Camera, Comments, and tab navigation

---
+++ InstaClone/InstaClone/InstaClone/ViewControllers/CommentVC.swift
@@ -0,0 +1,20 @@
+//
+//  CommentVC.swift
+//  InstaClone
+//
+//  Created by Daniel Bacon on 8/23/17.
+//  Copyright © 2017 Daniel Bacon. All rights reserved.
+//
+
+import UIKit
+
+class CommentVC: UIViewController {
+    
+    @IBOutlet weak var commentTextField: UITextField!
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
